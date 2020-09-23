447179497fc61a098238b456579df016bc34852b
Benjamin Sautner <ben@sautner.me>
2020-09-23 15:56:51 -0400

    h2

--- JediTerm/src/main/kotlin/com/jediterm/app/data/H2.kt
+++ JediTerm/src/main/kotlin/com/jediterm/app/data/H2.kt
@@ -2,12 +2,40 @@ package com.jediterm.app.data
 
 import java.sql.Connection
 import java.sql.DriverManager
+import java.sql.PreparedStatement
+import java.util.*
 
 class H2() {
 
-    val conn : Connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "")
+    val conn : Connection = DriverManager.getConnection("jdbc:h2:file:~/test3.db", "sa", "")
+    private val IN = "INSERT INTO CMDS" +
+            "  (command) VALUES " +
+            " (?);"
+
+    val cc =
+       """
+create table IF NOT EXISTS CMDS
+(
+    COMMAND VARCHAR       not null,
+    COUNT   INT default 0 not null
+);
+
+create unique index IF NOT EXISTS CMDS2_UINDEX
+    on CMDS (COMMAND);
+       """.trimIndent()
+
 
     fun connect() {
 
+        val c = conn.prepareStatement(cc)
+        c.executeUpdate()
+        conn.commit()
+
+        val s = conn.prepareStatement(IN)
+        s.setString(1, "test ${UUID.randomUUID()}")
+        s.executeUpdate()
+        conn.commit()
+        conn.close()
     }
+
 }
--- JediTerm/src/main/test/com/jediterm/app/data/H2Test.kt
+++ JediTerm/src/main/test/com/jediterm/app/data/H2Test.kt
@@ -1,9 +1,14 @@
 package com.jediterm.app.data
 
+import org.junit.Test
+
 
 internal class H2Test {
 
     @Test
     fun connect() {
+        val h2 = H2()
+        h2.connect()
+
     }
 }
