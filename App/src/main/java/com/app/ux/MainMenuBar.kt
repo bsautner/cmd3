5de6dea643a050190d711292c8468ec428ed1ca0
Benjamin Sautner <ben@sautner.me>
2020-09-28 12:41:01 -0400

    menus

---
+++ App/src/main/java/com/app/ux/MainMenuBar.kt
@@ -0,0 +1,28 @@
+package com.app.ux
+
+import javax.swing.JMenu
+import javax.swing.JMenuBar
+import javax.swing.JMenuItem
+
+
+class MainMenuBar(val selectionListener: SelectionListener) : JMenuBar() {
+
+
+    init {
+        val menu = JMenu("File")
+
+        val exitMenuItem = JMenuItem("Exit")
+        exitMenuItem.addChangeListener {
+
+        }
+
+        menu.add(exitMenuItem)
+
+
+        add(menu)
+
+
+
+    }
+
+}
